d06926519dd9c67dac8d4e5f09a28530502c1ca4
Spike Ganush <florian.jourdain@gmail.com>
2021-08-04 12:57:27 +1000

    Update script

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/auth', require('./routes/auth'))
 app.use(errorHandler)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path.join(__dirname, '/client/build')))
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
