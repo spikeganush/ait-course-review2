5c8f9307e7488a9e136d5076bc74b0a164524a9c
Spike Ganush <florian.jourdain@gmail.com>
2021-08-04 11:50:48 +1000

    home page + css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,8 +2,182 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='home'>
-      <div className='main'>Home</div>
+    <div className="content">
+      <div className="banner">
+        <h1>Enjoy Your New Experiences With Us</h1>
+        <img src="BANNER_IMAGE" alt="banner-image" className="banner" />
+      </div>
+
+      <input type="text" className="search-bar" />
+
+      <div className="welcome">
+        <h1>Welcome To Course Reviews</h1>
+        <p>
+          Welcome AIT students, let's explore our new websites that providing
+          lots of reviews and suggestions from AIT students. Students cann
+          freely give opinions in what the thing of some particular course and
+          share their experiences to another students.
+        </p>
+
+        <img src="URL_RIGHT_PHOTO" alt="right-photo" className="right-photo" />
+      </div>
+
+      <div className="suggestions">
+        <h1>Course Suggestions</h1>
+        <a href="view_all">View all</a>
+
+        <div className="course-card">
+          <img
+            src="URL_COURSE_PHOTO"
+            alt="course-photo"
+            className="course-photo"
+          />
+          <h3>Bachelor of IT</h3>
+          <p>Bachelor of Information Technology</p>
+          <div className="star-ranking">
+            <div className="star">
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img
+                src="https://i.ibb.co/TwYYb93/star-1.png"
+                alt="star-uncheckd"
+              />
+              <img
+                src="https://i.ibb.co/TwYYb93/star-1.png"
+                alt="star-uncheckd"
+              />
+            </div>
+            <div className="number-ranking">(120)</div>
+          </div>
+        </div>
+
+        <div className="course-card">
+          <img
+            src="URL_COURSE_PHOTO"
+            alt="course-photo"
+            className="course-photo"
+          />
+          <h3>Bachelor of Art</h3>
+          <p>Including with 2d, 3d, animations,...</p>
+          <div className="star-ranking">
+            <div className="star">
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img
+                src="https://i.ibb.co/TwYYb93/star-1.png"
+                alt="star-uncheckd"
+              />
+              <img
+                src="https://i.ibb.co/TwYYb93/star-1.png"
+                alt="star-uncheckd"
+              />
+            </div>
+            <div className="number-ranking">(20)</div>
+          </div>
+        </div>
+
+        <div className="course-card">
+          <img
+            src="URL_COURSE_PHOTO"
+            alt="course-photo"
+            className="course-photo"
+          />
+          <h3>Bachelor of Computer Science</h3>
+          <p>Java,Data base, Swift,...</p>
+          <div className="star-ranking">
+            <div className="star">
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img
+                src="https://i.ibb.co/TwYYb93/star-1.png"
+                alt="star-uncheckd"
+              />
+            </div>
+            <div className="number-ranking">(1120)</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="suggestions">
+        <h1>Most Reviews Subject</h1>
+        <a href="view_all">View all</a>
+
+        <div className="course-card">
+          <img
+            src="URL_COURSE_PHOTO"
+            alt="course-photo"
+            className="course-photo"
+          />
+          <h3>Advanced Web</h3>
+          <p>JavaScript, PHP, Front end,...</p>
+          <div className="star-ranking">
+            <div className="star">
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+            </div>
+            <div className="number-ranking">(120)</div>
+          </div>
+        </div>
+
+        <div className="course-card">
+          <img
+            src="URL_COURSE_PHOTO"
+            alt="course-photo"
+            className="course-photo"
+          />
+          <h3>System Analysis And Design</h3>
+          <p>The theory of system,...</p>
+          <div className="star-ranking">
+            <div className="star">
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img
+                src="https://i.ibb.co/TwYYb93/star-1.png"
+                alt="star-uncheckd"
+              />
+              <img
+                src="https://i.ibb.co/TwYYb93/star-1.png"
+                alt="star-uncheckd"
+              />
+              <img
+                src="https://i.ibb.co/TwYYb93/star-1.png"
+                alt="star-uncheckd"
+              />
+            </div>
+            <div className="number-ranking">(420)</div>
+          </div>
+        </div>
+
+        <div className="course-card">
+          <img
+            src="URL_COURSE_PHOTO"
+            alt="course-photo"
+            className="course-photo"
+          />
+          <h3>Java</h3>
+          <p>Java course.</p>
+          <div className="star-ranking">
+            <div className="star">
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img src="https://i.ibb.co/2kW5mnM/star.png" alt="star-checked" />
+              <img
+                src="https://i.ibb.co/TwYYb93/star-1.png"
+                alt="star-uncheckd"
+              />
+            </div>
+            <div className="number-ranking">(120)</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
