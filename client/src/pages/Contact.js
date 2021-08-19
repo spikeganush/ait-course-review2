0d55313f01eda5f43eac7b4d993806d7eb1edcdc
Spike Ganush <florian.jourdain@gmail.com>
2021-08-19 20:17:59 +1000

    email

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,7 +1,37 @@
 import React from 'react'
+import emailjs from 'emailjs-com'
 
-const Contact = () => {
-  return <div>Contact</div>
+export default function Contact() {
+  function sendEmail(e) {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_0rqea58',
+        'template_r1wofov',
+        e.target,
+        'user_H0rv5QcnQYhVfP5EInV0u'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+  }
+
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  )
 }
-
-export default Contact
