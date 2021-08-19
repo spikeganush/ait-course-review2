ea5dc3a5a514ac26cbd81f0d3df58f7412039676
Tiger <tiger.chalernphon15@gmail.com>
2021-08-19 18:57:53 +1000

    new commit

--- client/src/pages/Courses.js
+++ client/src/pages/Courses.js
@@ -1,13 +1,12 @@
 import React, { useEffect } from 'react'
-import Footer from '../components/Footer'
+//import Footer from '../components/Footer'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllCourses } from '../actions/course.action'
 import { isEmpty } from '../components/Utils'
 import { useHistory } from 'react-router-dom'
 import ReactStars from 'react-stars'
 
-const Courses = () => 
-{
+const Courses = () => {
   const history = useHistory()
   const coursesData = useSelector((state) => state.courseReducer)
   const dispatch = useDispatch()
@@ -39,93 +38,44 @@ const Courses = () =>
   }
 
   return (
-    <>
-      <main className="course-content">
-        <div className="row-course">
-              <div className="column-course">
-                <input
-                      type="text"
-                      className="course-input-bar"
-                      placeholder="Search by keyword"
-                    />
-              </div>
-              <div className="column-course1">
-                <div className="search-dropdown active">
-                  <ul className="dropdown-list">
-                    <li>Filters</li>
-                    <ul className="select-dropdown">
-                      <li>Diploma</li>
-                      <li>Postgraduate</li>
-                      <li>Undergraduate</li>
-                    </ul>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="row1">
-            <div className="column1">
-              <h3>Most Reviews Subject</h3>
-            </div>
-            <div className="column1">
-              <h1>Courses</h1>
-            </div>
-          </div>
-
-        <div className="row3">
-        {!isEmpty(coursesData[0]) &&
-          coursesData.map((course) => {
-            return (
-              <div
-                className="column3"
-                onClick={() => handleOpenCourse(course._id)}
-              >
-                <img
-                  src={__dirname + course.photo}
-                  alt="course"
-                  className="course-photo"
+    <div className="row3">
+      {!isEmpty(coursesData[0]) &&
+        coursesData.map((course) => {
+          return (
+            <div
+              className="column3"
+              onClick={() => handleOpenCourse(course._id)}
+              key={course._id}
+            >
+              <img
+                src={__dirname + course.photo}
+                alt="course"
+                className="course-photo"
+              />
+              <h3>{course.title}</h3>
+              <p>{course.summarize}</p>
+              <div className="star-ranking">
+                <ReactStars
+                  size={40}
+                  value={average(course._id)}
+                  edit={false}
                 />
-                <h3>{course.title}</h3>
-                <p>{course.summarize}</p>
-                <div className="star-ranking">
-                  <img
-                    src="https://i.ibb.co/2kW5mnM/star.png"
-                    alt="star-checked"
-                    className="star-image"
-                  />
-                  <img
-                    src="https://i.ibb.co/2kW5mnM/star.png"
-                    alt="star-checked"
-                    className="star-image"
-                  />
-                  <img
-                    src="https://i.ibb.co/2kW5mnM/star.png"
-                    alt="star-checked"
-                    className="star-image"
-                  />
-                  <img
-                    src="https://i.ibb.co/TwYYb93/star-1.png"
-                    alt="star-unchecked"
-                    className="star-image"
-                  />
-                  <img
-                    src="https://i.ibb.co/TwYYb93/star-1.png"
-                    alt="star-unchecked"
-                    className="star-image"
-                  />
-                  
-                  <div className="number-ranking">(120)</div>
-                </div>
+                {console.log(average(course._id))}
+                {course.reviews
+                  ? course.reviews.length > 0 && (
+                      <div className="read-review">
+                        ({course.reviews.length})
+                      </div>
+                    )
+                  : '(0)'}
               </div>
-            )
-          })}
-        </div> 
-      </main>
-      <Footer />
-    </>
-      
-    
+            </div>
+          )
+        })}
+    </div>
   )
 }
 
 export default Courses
+
 
