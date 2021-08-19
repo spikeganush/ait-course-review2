488d9090ce10433cddd227c5a0c2889fecc5c328
Spike Ganush <florian.jourdain@gmail.com>
2021-08-19 14:03:01 +1000

    email form

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+//import { send } from 'emailjs-com'
 
 const Contact = () => {
-  return <div>Contact</div>
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: '',
+    message: '',
+    reply_to: '',
+  })
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    {
+      /* --- METHOD TO SEND THE MAIL --- */
+    }
+  }
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value })
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="from_name"
+          placeholder="from name"
+          value={toSend.from_name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="to_name"
+          placeholder="to name"
+          value={toSend.to_name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="message"
+          placeholder="Your message"
+          value={toSend.message}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="reply_to"
+          placeholder="Your email"
+          value={toSend.reply_to}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default Contact
