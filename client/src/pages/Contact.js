ea1a0ce013e6e7b082a7fed70ae5a76ec87d8a3e
Spike Ganush <florian.jourdain@gmail.com>
2021-08-19 22:42:04 +1000

    Juan contact

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,37 +1,74 @@
 import React from 'react'
 import emailjs from 'emailjs-com'
+import { Form, Input, TextArea, Button } from 'semantic-ui-react'
+import Swal from 'sweetalert2'
 
-export default function Contact() {
-  function sendEmail(e) {
+import 'semantic-ui-css/semantic.min.css'
+import '../styles/Contact.css'
+
+const SERVICE_ID = 'service_4bwfcnh'
+const TEMPLATE_ID = 'template_ibwudej'
+const USER_ID = 'user_xRZGSoGvUHvHFegi5mPuy'
+
+const Contact = () => {
+  const handleOnSubmit = (e) => {
     e.preventDefault()
-
-    emailjs
-      .sendForm(
-        'service_0rqea58',
-        'template_r1wofov',
-        e.target,
-        'user_H0rv5QcnQYhVfP5EInV0u'
-      )
-      .then(
-        (result) => {
-          console.log(result.text)
-        },
-        (error) => {
-          console.log(error.text)
-        }
-      )
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
+      (result) => {
+        console.log(result.text)
+        Swal.fire({
+          icon: 'success',
+          title: 'Message Sent Successfully',
+        })
+      },
+      (error) => {
+        console.log(error.text)
+        Swal.fire({
+          icon: 'error',
+          title: 'Ooops, something went wrong',
+          text: error.text,
+        })
+      }
+    )
+    e.target.reset()
   }
 
   return (
-    <form className="contact-form" onSubmit={sendEmail}>
-      <input type="hidden" name="contact_number" />
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="contact-container">
+      <Form onSubmit={handleOnSubmit}>
+        <Form.Field
+          id="form-input-control-email"
+          control={Input}
+          label="Email"
+          name="user_email"
+          placeholder="Email…"
+          required
+          icon="mail"
+          iconPosition="left"
+        />
+        <Form.Field
+          id="form-input-control-last-name"
+          control={Input}
+          label="Name"
+          name="user_name"
+          placeholder="Name…"
+          required
+          icon="user circle"
+          iconPosition="left"
+        />
+        <Form.Field
+          id="form-textarea-control-opinion"
+          control={TextArea}
+          label="Message"
+          name="user_message"
+          placeholder="Message…"
+          required
+        />
+        <Button type="submit" color="green">
+          Submit
+        </Button>
+      </Form>
+    </div>
   )
 }
+export default Contact
